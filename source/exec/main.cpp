ea4b14dcae27d0f9945ab22b7642fc6c487554d4
Venkataram Edavamadathil Sivaram <vesion4690@gmail.com>
2025-01-30 15:42:58 -0800

    Parameterize parser abstractions by Token

--- source/exec/main.cpp
+++ source/exec/main.cpp
@@ -9,6 +9,7 @@
 #include <howler.hpp>
 
 #include "common/variant.hpp"
+#include "common/nabu.hpp"
 
 std::string source = R"(
 { $a in Real, $b in Real } := $(a + b = b + a)
@@ -16,57 +17,6 @@ predicate = { $a in Real, $b in Real }
 statement = $(a + b = b + a)
 predicate := statement
 )";
-
-template <typename F>
-struct __optional_returner : std::false_type {};
-
-template <typename R, typename ... Args>
-struct __optional_returner <std::function <std::optional <R> (Args...)>> : std::true_type {
-	using result = R;
-};
-
-#define hacked(T) *reinterpret_cast <T *> ((void *) nullptr)
-
-template <typename F>
-using optional_returner = __optional_returner <decltype(std::function(hacked(F)))>;
-
-template <typename F>
-constexpr bool optional_returner_v = optional_returner <F> ::value;
-
-template <size_t N>
-struct string_literal {
-	char value[N];
-
-	constexpr string_literal(const char (&str)[N]) {
-		std::copy_n(str, N, value);
-	}
-};
-
-template <size_t N>
-std::optional <std::string> raw_expanded(const string_literal <N> &raw, const std::string &source, size_t &i)
-{
-	for (size_t n = 0; n < N - 1; n++) {
-		if (source[n + i] != raw.value[n])
-			return std::nullopt;
-	}
-
-	i += N - 1;
-
-	return "?";
-}
-
-template <string_literal s, typename T>
-std::optional <T> raw(const std::string &source, size_t &i)
-{
-	if (auto r = raw_expanded(s, source, i)) {
-		if constexpr (std::is_constructible_v <T, std::string>)
-			return T(r.value());
-		else
-			return T();
-	}
-
-	return std::nullopt;
-}
 
 // Tokens
 struct null {};
@@ -114,85 +64,6 @@ std::optional <misc_identifier> identifier(const std::string &source, size_t &i)
 	return misc_identifier(id);
 }
 
-template <typename F>
-concept lexer_fn = optional_returner_v <F>
-		&& requires(const F &f,
-			    const std::string &source,
-			    size_t &i) {
-	{ f(source, i) };
-};
-
-template <typename T, typename... Rest>
-constexpr bool has_duplicates()
-{
-	if constexpr (sizeof...(Rest) == 0)
-		return false;
-	else
-		return (std::is_same_v <T, Rest> || ...) || has_duplicates <Rest...> ();
-}
-
-static_assert(has_duplicates <int, double, float, char> () == false);
-static_assert(has_duplicates <int, double, int, char> () == true);
-
-template <lexer_fn ... Fs>
-constexpr bool valid_lexer_group()
-{
-	return !has_duplicates <typename optional_returner <Fs> ::result...> ();
-}
-
-template <lexer_fn ... Fs>
-struct lexer_group {
-	static_assert(valid_lexer_group <Fs...> (),
-		"lexers in lexer_group(...) "
-		"must produce distinct types");
-
-	// TODO: variant...
-	using element_t = variant <typename optional_returner <Fs> ::result...>;
-	using result_t = std::vector <element_t>;
-
-	std::tuple <Fs...> fs;
-
-	lexer_group(const Fs &... fs_) : fs(fs_...) {}
-
-	// TODO: compare element index...
-	
-	template <size_t I>
-	bool eval_i(result_t &result, const std::string &source, size_t &i) const {
-		if constexpr (I >= sizeof...(Fs)) {
-			return false;
-		} else {
-			auto fv = std::get <I> (fs)(source, i);
-			if (fv) {
-				result.push_back(fv.value());
-				return true;
-			}
-
-			return eval_i <I + 1> (result, source, i);
-		}
-	}
-
-	std::optional <result_t> operator()(const std::string &s, size_t &i) const {
-		size_t old = i;
-
-		result_t result;
-
-		while (eval_i <0> (result, s, i)) {}
-
-		if (i == s.size())
-			return result;
-
-		i = old;
-
-		return std::nullopt;
-	}
-};
-
-template <lexer_fn ... Fs>
-auto lexer(const Fs &... args)
-{
-	return lexer_group(std::function(args)...);
-}
-
 static const auto oxidius_lexer = lexer(space,
 	raw <"{", sym_left_brace>,
 	raw <"}", sym_right_brace>,
@@ -206,32 +77,32 @@ static const auto oxidius_lexer = lexer(space,
 	raw <",", sym_comma>,
 	identifier);
 
-using Token = typename decltype(oxidius_lexer)::element_t;
+using OxidiusToken = typename decltype(oxidius_lexer)::element_t;
 
-std::string format_as(const Token &token)
+std::string format_as(const OxidiusToken &token)
 {
 	switch (token.index()) {
-	variant_case(Token, kwd_in):
+	variant_case(OxidiusToken, kwd_in):
 		return "<kwd: 'in'>";
-	variant_case(Token, sym_dollar):
+	variant_case(OxidiusToken, sym_dollar):
 		return "<sym: '$'>";
-	variant_case(Token, sym_colon_equals):
+	variant_case(OxidiusToken, sym_colon_equals):
 		return "<sym: ':='>";
-	variant_case(Token, sym_equals):
+	variant_case(OxidiusToken, sym_equals):
 		return "<sym: '='>";
-	variant_case(Token, sym_left_brace):
+	variant_case(OxidiusToken, sym_left_brace):
 		return "<sym: '{'>";
-	variant_case(Token, sym_right_brace):
+	variant_case(OxidiusToken, sym_right_brace):
 		return "<sym: '}'>";
-	variant_case(Token, sym_left_paren):
+	variant_case(OxidiusToken, sym_left_paren):
 		return "<sym: '('>";
-	variant_case(Token, sym_right_paren):
+	variant_case(OxidiusToken, sym_right_paren):
 		return "<sym: ')'>";
-	variant_case(Token, sym_plus):
+	variant_case(OxidiusToken, sym_plus):
 		return "<sym: '+'>";
-	variant_case(Token, sym_comma):
+	variant_case(OxidiusToken, sym_comma):
 		return "<sym: ','>";
-	variant_case(Token, misc_identifier):
+	variant_case(OxidiusToken, misc_identifier):
 		return "<identifier: '" + token.as <misc_identifier> () + "'>";
 	}
 
@@ -262,117 +133,7 @@ struct predicates {
 	std::vector <predicate_variable_domain> domains;
 };
 
-template <typename F, typename T>
-concept parser_fn = optional_returner_v <F>
-		&& requires(const F &f,
-			    const std::vector <T> &tokens,
-			    size_t &i) {
-	{ f(tokens, i) };
-};
-
-template <parser_fn <Token> ... Fs>
-struct parser_chain {
-	using result_t = std::tuple <typename optional_returner <Fs> ::result...>;
-
-	std::tuple <Fs...> fs;
-
-	parser_chain(const Fs &... fs_) : fs(fs_...) {}
-
-	template <size_t I>
-	bool eval_i(result_t &result, const std::vector <Token> &tokens, size_t &i) const {
-		if constexpr (I >= sizeof...(Fs)) {
-			return true;
-		} else {
-			auto fv = std::get <I> (fs)(tokens, i);
-			if (!fv)
-				return false;
-
-			if (!eval_i <I + 1> (result, tokens, i))
-				return false;
-
-			std::get <I> (result) = fv.value();
-
-			return true;
-		}
-	}
-
-	std::optional <result_t> operator()(const std::vector <Token> &tokens, size_t &i) const {
-		size_t c = i;
-		result_t result;
-
-		if (!eval_i <0> (result, tokens, i)) {
-			c = i;
-			return std::nullopt;
-		}
-
-		return result;
-	}
-};
-
-template <parser_fn <Token> ... Fs>
-auto chain(const Fs &... args)
-{
-	return parser_chain(std::function(args)...);
-}
-
-template <parser_fn <Token> F, bool EmptyOk, typename D = void>
-struct parser_loop {
-
-};
-
-// With a delimiter
-template <parser_fn <Token> F, bool EmptyOk, parser_fn <Token> D>
-struct parser_loop <F, EmptyOk, D> {
-	using result_t = std::vector <typename optional_returner <F> ::result>;
-
-	F f;
-	D d;
-
-	parser_loop(const F &f_, const D &d_) : f(f_), d(d_) {}
-
-	std::optional <result_t> operator()(const std::vector <Token> &tokens, size_t &i) const {
-		size_t c = i;
-		result_t result;
-
-		while (true) {
-			auto fv = f(tokens, i);
-			if (!fv) {
-				if (result.empty() && EmptyOk)
-					break;
-
-				i = c;
-				return std::nullopt;
-			}
-
-			result.push_back(fv.value());
-
-			if (!d(tokens, i))
-				break;
-		}
-
-		return result;
-	}
-};
-
-template <bool EmptyOk, parser_fn <Token> F, parser_fn <Token> D>
-auto loop(const F &f, const D &d)
-{
-	auto ff = std::function(f);
-	auto fd = std::function(d);
-	return parser_loop <decltype(ff), EmptyOk, decltype(fd)> (ff, fd);
-}
-
-template <typename T>
-requires (Token::type_index <T> () >= 0)
-std::optional <T> parse_token(const std::vector <Token> &tokens, size_t &i)
-{
-	if (!tokens[i].is <T> ())
-		return std::nullopt;
-
-	return tokens[i++].as <T> ();
-}
-
-std::optional <pure_variable> parse_pure_variable(const std::vector <Token> &tokens, size_t &i)
+std::optional <pure_variable> parse_pure_variable(const std::vector <OxidiusToken> &tokens, size_t &i)
 {
 	if (!tokens[i].is <sym_dollar> ())
 		return std::nullopt;
@@ -387,9 +148,9 @@ std::optional <pure_variable> parse_pure_variable(const std::vector <Token> &tok
 	return pure_variable(s);
 }
 
-std::optional <Expression> parse_expression(const std::vector <Token> &tokens, size_t &i)
+std::optional <Expression> parse_expression(const std::vector <OxidiusToken> &tokens, size_t &i)
 {
-	if (auto r = parse_token <misc_identifier> (tokens, i)) {
+	if (auto r = parse_token <OxidiusToken, misc_identifier> (tokens, i)) {
 		std::string s = r.value();
 		return reference(s);
 	}
@@ -397,10 +158,10 @@ std::optional <Expression> parse_expression(const std::vector <Token> &tokens, s
 	return std::nullopt;
 }
 
-std::optional <predicate_variable_domain> parse_predicate_domain(const std::vector <Token> &tokens, size_t &i)
+std::optional <predicate_variable_domain> parse_predicate_domain(const std::vector <OxidiusToken> &tokens, size_t &i)
 {
-	if (auto r = chain(parse_pure_variable,
-			   parse_token <kwd_in>,
+	if (auto r = chain <OxidiusToken> (parse_pure_variable,
+			   parse_token <OxidiusToken, kwd_in>,
 			   parse_expression)(tokens, i)) {
 		auto v = r.value();
 
@@ -413,12 +174,11 @@ std::optional <predicate_variable_domain> parse_predicate_domain(const std::vect
 	return std::nullopt;
 }
 
-std::optional <predicates> parse_predicates(const std::vector <Token> &tokens, size_t &i)
+std::optional <predicates> parse_predicates(const std::vector <OxidiusToken> &tokens, size_t &i)
 {
-	if (auto r = chain(parse_token <sym_left_brace>,
-			   loop <true> (parse_predicate_domain,
-			   		parse_token <sym_comma>),
-			   parse_token <sym_right_brace>)(tokens, i)) {
+	if (auto r = chain <OxidiusToken> (parse_token <OxidiusToken, sym_left_brace>,
+			   loop <OxidiusToken, true> (parse_predicate_domain, parse_token <OxidiusToken, sym_comma>),
+			   parse_token <OxidiusToken, sym_right_brace>)(tokens, i)) {
 		auto v = r.value();
 
 		return predicates {
