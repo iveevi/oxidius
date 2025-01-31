2ab9c93735e12b33c47e6f1134700d47641bb637
Venkataram Edavamadathil Sivaram <vesion4690@gmail.com>
2025-01-31 13:41:35 -0800

    Organize code and add grammar specifications in comments

--- source/exec/main.cpp
+++ source/exec/main.cpp
@@ -1,6 +1,5 @@
 #include <string>
 #include <vector>
-#include <optional>
 
 #include <fmt/format.h>
 #include <fmt/printf.h>
@@ -9,7 +8,9 @@
 
 #include "token.hpp"
 
-// Parsing constructions
+/////////////////////////
+// Parsing productions //
+/////////////////////////
 
 // Mathematical variable
 struct PureVariable : std::string {};
@@ -50,105 +51,91 @@ struct Import {
 };
 
 // Variable values
-// TODO: options(...)
-struct Value : bestd::variant <PurePredicates, Statement> {};
-
-struct Assignment {
-	Reference destination;
-	Value value;
-};
-
 struct Association {
 	// TODO: vector
 	Statement statement;
 	Predicates predicates;
 };
 
-#define PARSER(F, T) static bestd::optional <T> F(const std::vector <Token> &tokens, size_t &i)
+struct Value : bestd::variant <PurePredicates, Statement, Association> {};
 
-template <typename T, size_t ... Is, typename A>
-T __construct(const A &arg)
-{
-	if constexpr (sizeof...(Is) > 0)
-		return T(arg);
-	else
-		return T();
-}
-
-template <typename T, size_t ... Is, typename ... Args>
-T __construct(const bestd::tuple <Args...> &arg)
-{
-	return T(std::get <Is> (arg)...);
-}
-
-template <typename T, size_t ... Is>
-auto construct = [](const auto &arg)
-{
-	return __construct <T, Is...> (arg);
+struct Assignment {
+	Reference destination;
+	Value value;
 };
 
-template <typename T, size_t ... Is>
-auto feed_construct = [](const auto &arg)
-{
-	return arg.feed(construct <T, Is...>);
-};
+struct Instruction : bestd::variant <Import, Assignment, Value> {};
 
-template <typename F, typename G>
-constexpr auto compose(const F &f, const G &g)
-{
-	return [f, g](auto &&... args) -> decltype(auto) {
-        	return std::invoke(f, std::invoke(g, std::forward <decltype(args)> (args)...));
-	};
-}
-
-template <typename T, size_t ... Is, typename G>
-constexpr auto constructor(const G &g)
-{
-	return compose(feed_construct <T, Is...>, g);
-}
+//////////////////////////////////
+// Parser grammar specification //
+//////////////////////////////////
 
 #define PRODUCTION(T) inline const std::function <bestd::optional <T> (const std::vector <Token> &, size_t &)>
 
 struct Parser : nabu::TokenParser <Token> {
+	// reference := identifier
 	static PRODUCTION(Reference) reference = constructor <Reference, 0> (singlet <identifier>);
 	
+	// pure_variable := identifier
 	static PRODUCTION(PureVariable) pure_variable = constructor <PureVariable, 0> (singlet <identifier>);
 	
+	// pure_variable := pure_variable
 	static PRODUCTION(PureFactor) pure_factor = constructor <PureFactor> (pure_variable);
 	
+	// pure_term := pure_factor
 	static PRODUCTION(PureTerm) pure_term = constructor <PureTerm> (pure_factor);
 
+	// pure_expression := pure_term '+' pure_term
 	static PRODUCTION(PureExpression) pure_expression = constructor <PureExpression> (chain(pure_term, sym_plus(), pure_term));
 	
+	// pure_statement := pure_expression '=' pure_expression
 	static PRODUCTION(PureStatement) pure_statement = constructor <PureStatement> (chain(pure_expression, sym_equals(), pure_expression));
 
+	// variable := reference | pure_variable
 	static PRODUCTION(Variable) __var_ref = constructor <Variable, 0> (reference);
 	static PRODUCTION(Variable) __var_pure = constructor <Variable, 1> (chain(sym_dollar(), pure_variable));
 	static PRODUCTION(Variable) variable = options(__var_ref, __var_pure);
 
+	// expression := reference
 	static PRODUCTION(Expression) expression = constructor <Expression, 0> (reference);
 
+	// statement := reference | '$' '(' pure_statement ')'
 	static PRODUCTION(Statement) __stmt_ref = constructor <Statement, 0> (reference);
 	static PRODUCTION(Statement) __stmt_pure = constructor <Statement, 2> (chain(sym_dollar(), sym_left_paren(), pure_statement, sym_right_paren()));
 	static PRODUCTION(Statement) statement = options(__stmt_ref, __stmt_pure);
 
+	// pure_predicate_domain := variable 'in' expression
 	static PRODUCTION(PurePredicateDomain) pure_predicate_domain = constructor <PurePredicateDomain, 0, 2> (chain(variable, kwd_in(), expression));
 
+	// pure_predicates := '{' ( pure_predicate_domain ','? )* '}'
 	static PRODUCTION(PurePredicates) pure_predicates = constructor <PurePredicates, 1> (chain(sym_left_brace(), loop <true> (pure_predicate_domain, sym_comma()), sym_right_brace()));
-
-	static PRODUCTION(Import) import = constructor <Import, 1, 3> (chain(kwd_from(), identifier(), kwd_use(), identifier()));
-
-	static PRODUCTION(Value) __value_pred = constructor <Value, 0> (pure_predicates);
-	static PRODUCTION(Value) __value_statement = constructor <Value, 0> (statement);
-	static PRODUCTION(Value) value = options(__value_pred, __value_statement);
 	
+	// predicates := reference | pure_predicates
 	static PRODUCTION(Predicates) __pred_ref = constructor <Predicates, 0> (reference);
 	static PRODUCTION(Predicates) __pred_pure = constructor <Predicates, 0> (pure_predicates);
 	static PRODUCTION(Predicates) predicates = options(__pred_ref, __pred_pure);
 
+	// import := 'from' identifier 'use' identifier
+	static PRODUCTION(Import) import = constructor <Import, 1, 3> (chain(kwd_from(), identifier(), kwd_use(), identifier()));
+
+	// association := statement '::' predicates
+	static PRODUCTION(Association) association = constructor <Association, 0, 2> (chain(statement, sym_double_colon(), predicates));
+
+	// TODO: reference
+	// value := association | pure_predicates | statement
+	static PRODUCTION(Value) __value_association = constructor <Value, 0> (association);
+	static PRODUCTION(Value) __value_pred = constructor <Value, 0> (pure_predicates);
+	static PRODUCTION(Value) __value_statement = constructor <Value, 0> (statement);
+	static PRODUCTION(Value) value = options(__value_association, __value_pred, __value_statement);
+
+	// assignment := reference '=' value
 	static PRODUCTION(Assignment) assignment = constructor <Assignment, 0, 2> (chain(reference, sym_equals(), value));
 
-	static PRODUCTION(Association) association = constructor <Association, 0, 2> (chain(statement, sym_double_colon(), predicates));
+	// instruction := import | assignment | value
+	static PRODUCTION(Instruction) __inst_import = constructor <Instruction, 0> (import);
+	static PRODUCTION(Instruction) __inst_assignment = constructor <Instruction, 0> (assignment);
+	static PRODUCTION(Instruction) __inst_value = constructor <Instruction, 0> (value);
+	static PRODUCTION(Instruction) instruction = options(__inst_import, __inst_assignment, __inst_value);
 };
 
 int main()
@@ -176,40 +163,6 @@ int main()
 
 	index = 0;
 
-	auto import = Parser::import(tokens, index);
-	
-	fmt::println("index: {}, next={}", index, tokens[index]);
-
-	fmt::println("\timport: {} <- {}", import->package, import->items);
-
-	Parser::statement(tokens, index);
-	
-	fmt::println("index: {}, next={}", index, tokens[index]);
-
-	Parser::singlet <sym_double_colon> (tokens, index);
-
-	fmt::println("index: {}, next={}", index, tokens[index]);
-
-	auto predicates = Parser::pure_predicates(tokens, index);
-
-	if (predicates.has_value()) {
-		fmt::println("domains ({}):", predicates->domains.size());
-		for (auto &pd : predicates->domains) {
-			fmt::println("\t{} -> {}", pd.var.as <PureVariable> (), pd.domain.as <Reference> ());
-		}
-	}
-
-	fmt::println("index: {}; next={}", index, tokens[index]);
-	
-	Parser::assignment(tokens, index);
-	
-	fmt::println("index: {}, next={}", index, tokens[index]);
-	
-	Parser::assignment(tokens, index);
-	
-	fmt::println("index: {}, next={}", index, tokens[index]);
-	
-	Parser::association(tokens, index);
-	
-	fmt::println("index: {}, next={} (end={})", index, tokens[index], index == tokens.size());
+	while (auto v = Parser::instruction(tokens, index))
+		fmt::println("instruction[{}], index={}, next={}", v.value().index(), index, tokens[index]);
 }
